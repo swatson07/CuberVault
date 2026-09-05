26a3826fdbcfb26d2dd82ef105c08e9ffa5d1d4c
Sam <samjwatson07@gmail.com>
2026-09-05 21:09:55 +0100

    Added comments to the scramble

--- Backend/scramblers.py
+++ Backend/scramblers.py
@@ -1,17 +1,20 @@
 import random
+from enum import Enum
 
+# Possible cube moves
 moves = ["U", "D", "L", "R", "F", "B",
          "U'", "D'", "L'", "R'", "F'", "B'",
          "U2", "D2", "L2", "R2", "F2", "B2"]
 
+
+# Checks if the current move is redundant 
 def is_redundant(move, prev):
-    """Checks if the current move repeats the same face as the previous move."""
     if prev is None:
         return False
     return move[0] == prev[0]
 
+# Generates a manoeuvre to scramble the cube 
 def full_scramble():
-    """Scrambles the Cube ready for solving."""
     scramble = []
     while len(scramble) < 21:
         move = random.choice(moves)
